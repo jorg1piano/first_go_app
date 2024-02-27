bf9d548d8873bfb248f16e41d330e18709d7b166
Jørgen Andersen <jorg1piano@gmail.com>
2024-02-27 22:31:07 +0100

    Start a server and print out the port number we are listening on

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/jorg1piano/rssapp
 
 go 1.22.0
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+)
--- main.go
+++ main.go
@@ -3,19 +3,35 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("hello")
+	fmt.Println("Starting server")
 
 	godotenv.Load(".env")
 
 	portstring := os.Getenv("PORT")
 	if portstring == "" {
 		log.Fatal("Port not set")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portstring,
+	}
+
+	log.Printf("Server starting on port %s", portstring)
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	fmt.Println("PORT: ", portstring)
