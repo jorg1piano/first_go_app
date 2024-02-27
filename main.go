d18c302b5b8a27b8fb8c4a2ce0155b6b059cc9b2
Jørgen Andersen <jorg1piano@gmail.com>
2024-02-27 22:20:16 +0100

    Load dot env file using godotenv

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("hello")
+
+	godotenv.Load(".env")
+
+	portstring := os.Getenv("PORT")
+	if portstring == "" {
+		log.Fatal("Port not set")
+	}
+
+	fmt.Println("PORT: ", portstring)
 }
